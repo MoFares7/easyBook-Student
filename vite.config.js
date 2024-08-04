bd1a9badfa5608d420adae739e3f60218e778db4
MoFares7 <faresdabbas1@gmail.com>
2024-08-04 17:43:47 +0300

    fix uploading

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 // import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
+
 
 // // https://vitejs.dev/config/
 // export default defineConfig({
@@ -7,7 +7,8 @@
 // })
 
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc'
+// import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
