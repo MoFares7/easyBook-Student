94f595b52bdb34f1ada1819c659eab3c0c62106f
MoFares7 <faresdabbas1@gmail.com>
2024-08-04 17:07:12 +0300

    final modify

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react-swc';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     host: true,
-//     port: 5173,
-//   }
-// });
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react-swc';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     // host: true,
-//     port: 5173,
-//     // strictPort: true,
-//   }
-// });
+  build: {
+    outDir: 'dist', 
+  },
+});
