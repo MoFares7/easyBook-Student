9b334d2efac43e2ece85227eabc6b2ac9cb67532
MoFares7 <faresdabbas1@gmail.com>
2024-08-04 18:27:58 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Ensure this matches the output directory Vercel expects
+    outDir: 'build', 
   },
 })
