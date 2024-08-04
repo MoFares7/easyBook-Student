947120b9362861f80075720e9dbb9cf39b51a050
MoFares7 <faresdabbas1@gmail.com>
2024-08-04 17:39:00 +0300

    Fix missing @vitejs/plugin-react dependency

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react-swc'
 
+// // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
 // })
@@ -10,7 +11,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
 });
